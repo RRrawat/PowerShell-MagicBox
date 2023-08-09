b3219f575623382fee8551b7155ca63ff01b85b4
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-08-09 13:57:33 +0530

    Update install-choco.ps1

--- install-choco.ps1
+++ install-choco.ps1
@@ -17,3 +17,23 @@ try {
 ####################3333
 # 1
 ########################
+function InstallChocolatey {
+  try {
+      # Chocolatey is not installed, so proceed with installation
+      Write-Host "Chocolatey is not installed. Installing now..."
+      if (Test-Path -path "C:\ProgramData\chocolatey"){
+        remove-item -path "C:\ProgramData\chocolatey" -Recurse -Force
+      } 
+      # Set the execution policy and download/install Chocolatey
+      Set-ExecutionPolicy RemoteSigned -Scope Process -Force
+      [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+      Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+      Write-Host "Chocolatey has been installed successfully."
+  }
+  catch {
+    <#Do this if a terminating exception happens#>
+    Write-Host "Failed to install Chocolatey."
+    Throw $_
+  }
+}
+InstallChocolatey
