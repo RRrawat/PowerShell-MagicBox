0055dae5b384899a17b8c26cd1598db937161e42
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-08-09 13:55:23 +0530

    Update install-choco.ps1

--- install-choco.ps1
+++ install-choco.ps1
@@ -1,1 +1,11 @@
+# Installs Chocolatey (needs admin rights)
+#Requires -RunAsAdministrator
 
+try {
+	[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+	iwr https://community.chocolatey.org/install.ps1 -UseBasicParsing | iex
+	exit 0 # success
+} catch {
+	" Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
